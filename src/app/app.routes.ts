b6a565b71bef9c7793adab9f06847641c78d2fc6
Sebastian Cabrera <ccabreram1@est.ups.edu.ec>
2025-11-08 17:38:45 -0500

    navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,15 +2,28 @@ import { Routes } from '@angular/router';
 import { Component } from '@angular/core';
 import { Homepage } from './features/homepage/homepage';
 import { PerfilPage } from './features/PerfilPage/PerfilPage';
+import { Proyectos } from './features/proyectos/proyectos';
+import { Proyectos2 } from './features/proyectos2/proyectos2';
 
 export const routes: Routes = [
     {
         path: "",
-        component: Homepage,
+        component: Homepage,    
     },
 
     {
         path: 'perfil',
         component: PerfilPage,
+    },
+
+    {
+        path: 'proyectos',
+        component: Proyectos,
+    },
+
+    {
+        path: 'proyectos2',
+        component: Proyectos2,
     }
 ];
+    
